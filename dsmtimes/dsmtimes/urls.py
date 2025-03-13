419a321222b13c96d4c08160135c10c93972d261
Beatus Simon <129729984+beatussimon@users.noreply.github.com>
2025-03-13 11:07:48 +0000

    correct project urls file

--- dsmtimes/dsmtimes/urls.py
+++ dsmtimes/dsmtimes/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from core.admin import admin_site
 
 urlpatterns = [
-    path('admin/', admin_site.urls),
+    path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
