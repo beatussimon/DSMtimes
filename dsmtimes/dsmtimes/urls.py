8f63f3d42ee18d06eda0e0e3adc81ddb3352e9f5
Beatus Simon <129729984+beatussimon@users.noreply.github.com>
2025-03-13 11:12:40 +0000

    removed import admin site

--- dsmtimes/dsmtimes/urls.py
+++ dsmtimes/dsmtimes/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from core.admin import admin_site
 
 urlpatterns = [
     path('admin/', admin.site.urls),
